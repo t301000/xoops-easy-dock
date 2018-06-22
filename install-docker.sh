f8651764ee47234f5a5f69830b7000db9e7be520
林士立 <t301000@gmail.com>
2018-06-23 00:42:38 +0800

    新增用於 centos 之命令

--- install-docker.sh
+++ install-docker.sh
@@ -5,11 +5,19 @@
 
 docker_compose_version="1.21.2"
 
+lsb_dist=""
+# Every system that we officially support has /etc/os-release
+if [ -r /etc/os-release ]; then
+    lsb_dist="$(. /etc/os-release && echo "$ID")"
+fi
+
 # install docker
 echo "**** 安裝 docker ****"
 sleep 3
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+echo "執行 usermod -aG docker $(whoami)"
 sudo usermod -aG docker $(whoami)
 
 # install docker-compose
@@ -18,6 +26,12 @@ sleep 3
 curl -L https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m` -o docker-compose
 sudo mv docker-compose /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+if [ "$lsb_dist" == "centos" ]; then
+    echo "**** CentOS 啟用 docker daemon ****"
+    sudo systemctl enable docker
+    sudo systemctl start docker
+fi
 
 echo "**** 安裝完成 ****"
 echo "**** 請先重新登入 ****"
