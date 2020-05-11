46f75d1dfbdb71b74da870c8846dea74a856b9ee
林士立 <t301000@gmail.com>
2020-05-11 23:15:58 +0800

    自動抓取可用 php 版本並調整顯示

--- setup_tags.sh
+++ setup_tags.sh
@@ -71,15 +71,19 @@ if [[ "$ans" == "y" ]] || [[ "$ans" == "Y" ]] || [[ "$ans" == "" ]]; then
 fi
 
 
-printf "\n\n**** 以下各項目直接按 Enter 表示不做更改 ****\n\n"
+printf "\n\n\n**** 以下各項目  直接按 Enter  表示  不做更改 ****\n\n\n"
+pause "按任意鍵繼續 或 Ctrl + C 中斷...."
 
 # 設定 PHP_VERSION
-printf "設定 PHP 版本 PHP_VERSION\n"
+printf "\n\n==== 設定 PHP 版本 PHP_VERSION ====\n"
 CURRENT_PHP_VERSION=$(cat .env |grep ^PHP_VERSION=)
 printf "目前設定 $CURRENT_PHP_VERSION\n"
-printf "可用之值：7.0｜7.2｜7.3｜7.4\n"
+printf "可用之值：\n"
+# 由 php*.ini 抓取 php 版本的部份
+ls php-fpm/php*.ini | cut -c 12- | rev | cut -c 5- | rev
 ans=""
 my_php_main_version=$(echo $CURRENT_PHP_VERSION | cut -d'=' -f 2)
+printf "\n"
 read -p "新設定值： " ans
 if [[ "$ans" != "" ]]; then
     my_php_main_version=$ans
@@ -88,14 +92,14 @@ if [[ "$ans" != "" ]]; then
 fi
 
 # 設定 PHP_TAG
-printf "\n設定 PHP_TAG"
+printf "\n\n==== 設定 PHP_TAG ===="
 CURRENT_PHP_TAG=$(cat .env |grep ^PHP_TAG=)
 printf "\n目前設定 $CURRENT_PHP_TAG\n"
 ans=""
 printf "可用之 tag：\n"
 listTags t301000/xoops.easy.dock.php-fpm $my_php_main_version
 printf "\n"
-printf "完整可用之 tag 列表可至 https://hub.docker.com/r/t301000/xoops.easy.dock.php-fpm/tags 查詢\n"
+printf "完整 tag 列表可至 https://hub.docker.com/r/t301000/xoops.easy.dock.php-fpm/tags 查詢\n"
 read -p "新設定值： " ans
 if [[ "$ans" != "" ]]; then
     ans=PHP_TAG\=$ans
@@ -103,14 +107,14 @@ if [[ "$ans" != "" ]]; then
 fi
 
 # 設定 CADDY_TAG
-printf "\n設定 CADDY_TAG"
+printf "\n\n==== 設定 CADDY_TAG ===="
 CURRENT_CADDY_TAG=$(cat .env |grep ^CADDY_TAG=)
 printf "\n目前設定 $CURRENT_CADDY_TAG\n"
 ans=""
 printf "可用之 tag：\n"
 listTags t301000/xoops.easy.dock.caddy
 printf "\n"
-printf "完整可用之 tag 列表可至 https://hub.docker.com/r/t301000/xoops.easy.dock.caddy/tags 查詢\n"
+printf "完整 tag 列表可至 https://hub.docker.com/r/t301000/xoops.easy.dock.caddy/tags 查詢\n"
 read -p "新設定值： " ans
 if [[ "$ans" != "" ]]; then
     ans=CADDY_TAG\=$ans
@@ -118,21 +122,21 @@ if [[ "$ans" != "" ]]; then
 fi
 
 # 設定 MYSQL_TAG
-printf "\n設定 MYSQL_TAG"
+printf "\n\n==== 設定 MYSQL_TAG ===="
 CURRENT_MYSQL_TAG=$(cat .env |grep ^MYSQL_TAG=)
 printf "\n目前設定 $CURRENT_MYSQL_TAG\n"
 ans=""
 printf "可用之 tag：\n"
 listTags t301000/xoops.easy.dock.mysql
 printf "\n"
-printf "完整可用之 tag 列表可至 https://hub.docker.com/r/t301000/xoops.easy.dock.mysql/tags 查詢\n"
+printf "完整 tag 列表可至 https://hub.docker.com/r/t301000/xoops.easy.dock.mysql/tags 查詢\n"
 read -p "新設定值： " ans
 if [[ "$ans" != "" ]]; then
     ans=MYSQL_TAG\=$ans
     sed -i "s/$CURRENT_MYSQL_TAG/$ans/g" .env
 fi
 
-printf "\n*****************************"
+printf "\n\n*****************************"
 printf "\n以下是新的 image tag 設定："
 printf "\n*****************************\n\n"
 cat .env |grep ^PHP_VERSION=
@@ -151,7 +155,7 @@ if [[ "$ans" == "Y" ]]; then
     ./up.sh
     exit 0
 fi
-printf "\n你選擇不立即啟動服務/容器"
+printf "\n\n你選擇不立即啟動服務/容器"
 printf "\n\n*****************************"
 printf "\n若要啟動服務/容器以使變更生效，請依序執行以下指令："
 if [[ $SERVICE_STOPPED == false ]]; then
